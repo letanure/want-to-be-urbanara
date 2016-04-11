f8c76a2979f141f430011c76aa533067abc15689
letanure <letanure@gmail.com>
2016-04-11 22:43:35 +0200

    Add gulp task for markups

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,13 +5,16 @@ import plumber from 'gulp-plumber';
 import uglify from 'gulp-uglify';
 import rollup from 'gulp-rollup';
 import rollupConfig from './rollup.config';
+import jade from 'gulp-jade';
 
 const source = {
+  markups: 'app/markups/**/*.jade',
   scripts: 'app/scripts/main.js'
 };
 
 const build = {
-  scripts: '.build/scripts/',
+  markups: '.build/',
+  scripts: '.build/scripts/'
 };
 
 gulp.task('scripts', () => {
@@ -20,4 +23,11 @@ gulp.task('scripts', () => {
     .pipe(rollup(rollupConfig))
     .pipe(uglify())
     .pipe(gulp.dest(build.scripts));
+});
+
+gulp.task('markups', () => {
+  gulp.src(source.markups)
+    .pipe(plumber())
+    .pipe(jade())
+    .pipe(gulp.dest(build.markups));
 });
