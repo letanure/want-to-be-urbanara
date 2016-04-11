4614eefeb515f77b7545821b9aba77dd956f6fa4
letanure <letanure@gmail.com>
2016-04-11 22:27:46 +0200

    add gulp task for js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,23 @@
 'use strict';
 
 import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import uglify from 'gulp-uglify';
+import rollup from 'gulp-rollup';
+import rollupConfig from './rollup.config';
+
+const source = {
+  scripts: 'app/scripts/main.js'
+};
+
+const build = {
+  scripts: '.build/scripts/',
+};
+
+gulp.task('scripts', () => {
+  gulp.src(source.scripts)
+    .pipe(plumber())
+    .pipe(rollup(rollupConfig))
+    .pipe(uglify())
+    .pipe(gulp.dest(build.scripts));
+});
