8d8d685073216997004bc92320f8dd0766c28658
letanure <letanure@gmail.com>
2016-04-11 22:54:36 +0200

    Add gulp task for styles

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,15 +6,26 @@ import uglify from 'gulp-uglify';
 import rollup from 'gulp-rollup';
 import rollupConfig from './rollup.config';
 import jade from 'gulp-jade';
+import sourcemaps from 'gulp-sourcemaps';
+import stylus from 'gulp-stylus';
+import koutoSwiss from 'kouto-swiss';
+import prefixer from 'autoprefixer-stylus';
+import jeet from 'jeet';
+import rupture from 'rupture';
+import gcmq from 'gulp-group-css-media-queries';
+import cssnano from 'gulp-cssnano';
 
 const source = {
   markups: 'app/markups/**/*.jade',
-  scripts: 'app/scripts/main.js'
+  scripts: 'app/scripts/main.js',
+  styles: 'app/styles/**/*.styl',
+  stylesMain: 'app/styles/**/*.styl'
 };
 
 const build = {
   markups: '.build/',
-  scripts: '.build/scripts/'
+  scripts: '.build/scripts/',
+  styles: '.build/styles/'
 };
 
 gulp.task('scripts', () => {
@@ -30,4 +41,17 @@ gulp.task('markups', () => {
     .pipe(plumber())
     .pipe(jade())
     .pipe(gulp.dest(build.markups));
+});
+
+gulp.task('styles', () => {
+  gulp.src(source.stylesMain)
+    .pipe(sourcemaps.init())
+    .pipe(stylus({
+      use: [koutoSwiss(), prefixer(), jeet(), rupture()],
+      compress: true
+    }))
+    .pipe(gcmq())
+    .pipe(cssnano())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(build.styles));
 });
